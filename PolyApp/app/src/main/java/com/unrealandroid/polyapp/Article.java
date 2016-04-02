dd9dfdf9ef753ddd4f0e294baa4ebbf21e745346
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-02 15:40:13 +0200

    fix #2

---
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/Article.java
@@ -0,0 +1,17 @@
+package com.unrealandroid.polyapp;
+
+/**
+ * Created by Rémy Kaloustian on 02/04/2016.
+ */
+public class Article {
+
+    String _pathToImage;
+    String _title;
+    String _content;
+
+    public Article(String _pathToImage, String _title, String _content) {
+        this._pathToImage = _pathToImage;
+        this._title = _title;
+        this._content = _content;
+    }
+}//class Article
