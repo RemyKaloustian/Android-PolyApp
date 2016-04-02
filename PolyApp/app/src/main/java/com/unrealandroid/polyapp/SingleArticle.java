ceff7657c0de8562be92f27ca71be8af47e0cf5b
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-02 16:00:54 +0200

    Working on SIngle article

--- PolyApp/app/src/main/java/com/unrealandroid/polyapp/SingleArticle.java
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/SingleArticle.java
@@ -6,11 +6,14 @@ package com.unrealandroid.polyapp;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -20,13 +23,14 @@ import java.sql.SQLException;
  */
 public class SingleArticle extends Fragment{
 
+    private Article _article;
 
 
     public SingleArticle() throws IOException, SQLException {
         Intent intent = getActivity().getIntent();
         String id = intent.getStringExtra("id");
 
-        Article currentArticle = new DBHelper(getContext()).getArticle(id);
+        _article = new DBHelper(getContext()).getArticle(id);
 
     }
 
@@ -37,6 +41,22 @@ public class SingleArticle extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.single_article, container, false);
+
+
+        TextView title = (TextView)rootView.findViewById(R.id.ArticleTitle);
+        title.setText(_article._title);
+
+        TextView content = (TextView)rootView.findViewById(R.id.ArticleContent);
+        content.setText(_article._content);
+
+        ImageView image = (ImageView)rootView.findViewById(R.id.ArticleImage);
+
+        
+        image.setImageBitmap(new Bitmap());
+
+
+
+
         //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
         //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
         return rootView;
