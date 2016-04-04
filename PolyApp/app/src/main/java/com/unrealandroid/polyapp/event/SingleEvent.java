8dac7eb64808d733d7a63042531e6a8866faad5e
Kevin <kevin.duglue@etu.unice.fr>
2016-04-04 12:09:17 +0200

    Google Map

--- PolyApp/app/src/main/java/com/unrealandroid/polyapp/event/SingleEvent.java
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/event/SingleEvent.java
@@ -8,6 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.unrealandroid.polyapp.R;
 
 
@@ -16,6 +21,8 @@ import com.unrealandroid.polyapp.R;
  */
 public class SingleEvent extends AppCompatActivity {
 
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
@@ -23,7 +30,9 @@ public class SingleEvent extends AppCompatActivity {
         Intent intent = getIntent();
         int idEvent = intent.getIntExtra("IdEvent", 1);
         setContentView(R.layout.single_event);
-        TextView textView = (TextView) findViewById(R.id.textEventsss);
-        textView.setText("Coucou toi" + idEvent);
+
+        GoogleMap googleMap;
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        final LatLng TutorialsPoint = new LatLng(21 , 57);
     }
 }
