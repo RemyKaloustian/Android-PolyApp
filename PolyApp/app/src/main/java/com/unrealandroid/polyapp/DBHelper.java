a963c49f9bc9ea1204f429aa2fcc52e7ca625263
Charly Lafon <charlylafon@gmail.com>
2016-04-05 22:41:54 +0200

    Putain de bdd

--- PolyApp/app/src/main/java/com/unrealandroid/polyapp/DBHelper.java
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/DBHelper.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
     protected static String DB_PATH = "/data/data/" + BuildConfig.APPLICATION_ID + "/";
-    protected static String DB_NAME = "dbapps.db";
+    protected static String DB_NAME = "putainDeBDD.db";
 
     protected SQLiteDatabase myDataBase;
     private final Context myContext;
