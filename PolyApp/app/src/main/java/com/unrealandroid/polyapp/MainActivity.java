69688bc802d667a48490209f16cf4744e8e0f6a5
Kevin <kevin.duglue@etu.unice.fr>
2016-04-08 17:20:29 +0200

    Fix #15 Resolution Bug Scroll down

--- PolyApp/app/src/main/java/com/unrealandroid/polyapp/MainActivity.java
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/MainActivity.java
@@ -43,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int mUIFlag =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
