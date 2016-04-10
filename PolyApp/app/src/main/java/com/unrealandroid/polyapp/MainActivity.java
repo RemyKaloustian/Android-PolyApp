a9cf4cd5d20d69bf210b25ba35e259c6dd8be36b
Charly Lafon <charlylafon@gmail.com>
2016-04-10 23:02:29 +0200

    Add onResume

--- PolyApp/app/src/main/java/com/unrealandroid/polyapp/MainActivity.java
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/MainActivity.java
@@ -77,6 +77,25 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Used to set again in fullScreen mode when going on a detail event/project and then use "back"
+     */
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        int mUIFlag =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
