54ab75b71d0afc71e0901751985b8aff79aa3bd1
Kevin <kevin.duglue@etu.unice.fr>
2016-04-21 21:09:13 +0200

    Toolbar Event

--- PolyApp/app/src/main/java/com/unrealandroid/polyapp/event/SingleEvent.java
+++ PolyApp/app/src/main/java/com/unrealandroid/polyapp/event/SingleEvent.java
@@ -2,11 +2,18 @@ package com.unrealandroid.polyapp.event;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -31,6 +38,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 /**
@@ -45,7 +54,7 @@ public class SingleEvent extends AppCompatActivity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        Event event = intent.getParcelableExtra("Event");
+        final Event event = intent.getParcelableExtra("Event");
         setContentView(R.layout.single_event);
 
 
@@ -75,14 +84,41 @@ public class SingleEvent extends AppCompatActivity {
         }
 
 
-        final ScrollView scrollView = (ScrollView) findViewById(R.id.eventScrollView);
+        final NestedScrollView scrollView = (NestedScrollView) findViewById(R.id.eventScrollview);
         TextView title = (TextView) findViewById(R.id.titleSingleEvent);
         TextView content = (TextView) findViewById(R.id.contentSingleEvent);
         ImageView imageView = (ImageView) findViewById(R.id.imageSingleEvent);
 
-        setTitle(event.getTitle());
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.add_calendar);
+        //floatingActionButton.setBackgroundTintList(ColorStateList.valueOf(R.color.colorPrimary));
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, event.getTitle());
+                calIntent.putExtra(CalendarContract.Events.DESCRIPTION, event.getContent());
+
+                GregorianCalendar calDate = new GregorianCalendar(2016, 4, 21);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        calDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        calDate.getTimeInMillis());
+
+                startActivity(calIntent);
+            }
+        });
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("Bibu");
+        collapsingToolbar.setCollapsedTitleTextColor(R.color.white);
+
 
         if(bitmap != null)
             imageView.setImageBitmap(bitmap);
@@ -92,6 +128,9 @@ public class SingleEvent extends AppCompatActivity {
         title.setTypeface(null, Typeface.BOLD);
         content.setText(event.getContent());
 
+
+
+        /******* GOOGLE MAP *******/
 
         googleMap = ((ScrollableMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -109,6 +148,12 @@ public class SingleEvent extends AppCompatActivity {
                 .position(new LatLng(event.getLocation_lat(), event.getLocation_long()))
                 .title(event.getTitle()));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(event.getLocation_lat(), event.getLocation_long()), 15.0f));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // getMenuInflater().inflate(R.menu.sample_actions, menu);
+        return true;
     }
 
     @Override
